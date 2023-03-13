01936b183ab3468c0c2d783d8bf33a611d7b751c
Khaled Hosny <khaled@aliftype.com>
2023-03-13 09:22:51 +0200

    Mark all glyphs as input
    Allows FontTools to optimize this and use non-chaining contextual
    positioning lookups.

--- scripts/update-overhang-fea.py
+++ scripts/update-overhang-fea.py
@@ -77,8 +77,8 @@ def main(args):
                 found = True
 
                 match = glyphs[0]
-                lookahead = " ".join(glyphs[1:])
-                rules.append(f"\tpos {match}' {adjustment} {lookahead};")
+                lookahead = "' ".join(glyphs[1:])
+                rules.append(f"\tpos {match}' {adjustment} {lookahead}';")
             if not found:
                 break
             i += 1
